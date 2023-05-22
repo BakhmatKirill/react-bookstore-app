39be445acbe9373c2ce0455caf39bcf27abd0af2
111Bakhmat <k.bakhmat@mail.ru>
2023-05-22 20:19:21 +0300

    feat(pages) add file index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,21 @@
-//import arrowImage from "./icons/arrow.svg";
-//export { arrowImage };
+import { Account } from "./Account/Account";
+import { BookDetails } from "./BookDetails/Bookdetails";
+import { CartBooks } from "./CartBooks/CartBooks";
+import { FavoritsBooks } from "./FavoritesBooks/FavoritesBooks";
+import { Home } from "./Home/Home";
+import { NotFound } from "./NotFound/NotFound";
+import { Register } from "./Register/Register";
+import { SearchBooks } from "./SearchBooks/SearchBooks";
+import { SignIn } from "./SignIn/SignIn";
+
+export {
+  Account,
+  BookDetails,
+  CartBooks,
+  FavoritsBooks,
+  Home,
+  NotFound,
+  Register,
+  SearchBooks,
+  SignIn,
+};
