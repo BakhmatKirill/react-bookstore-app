58474ef28540946c330b6b78489c9002e537ae8a
111Bakhmat <k.bakhmat@mail.ru>
2023-05-25 22:40:42 +0300

    feat(ui): add color, media, theme, resetCSS files

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,2 +1,12 @@
-//import arrowImage from "./icons/arrow.svg";
-//export { arrowImage };
+import { Color } from "./colors";
+import { Media } from "./media";
+import { ResetCSS } from "./resetCSS";
+import { DarkTheme, LightTheme } from "./theme";
+
+export {
+  Color,
+  Media,
+  ResetCSS,
+  DarkTheme,
+  LightTheme,
+};
