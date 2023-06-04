c1a5e14931c74707b5b3a18994705e1af2abcaaf
111Bakhmat <k.bakhmat@mail.ru>
2023-06-04 12:15:06 +0300

    feat(types): add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,19 @@
-//import arrowImage from "./icons/arrow.svg";
-//export { arrowImage };
+import {
+  IBook,
+  INewBooks,
+  IResponseSearch,
+  IBookDetails,
+  ICartItem,
+  ISearchResult,
+  IBookISBN13,
+} from "./types";
+
+export type {
+  IBook,
+  INewBooks,
+  IBookDetails,
+  ICartItem,
+  IResponseSearch,
+  ISearchResult,
+  IBookISBN13,
+};
---
+++ src/types/types.ts
@@ -0,0 +1,70 @@
+export interface IBook {
+    image: string;
+    isbn13: string;
+    price: string;
+    subtitle: string;
+    title: string;
+    url: string;
+}
+
+export interface INewBooks {
+    error: string;
+    total: string;
+    books: IBook[];
+}
+
+export interface IBookDetails {
+    error: string;
+    title: string;
+    subtitle: string;
+    authors: string;
+    publisher: string;
+    language: string;
+    isbn10: string;
+    isbn13: string;
+    pages: string;
+    year: string;
+    rating: string;
+    desc: string;
+    price: string;
+    image: string;
+    url: string;
+    pdf?: {
+    [key: string]: string;
+  };
+}
+
+export interface ICartItem {
+    error: string;
+    title: string;
+    subtitle: string;
+    authors: string;
+    publisher: string;
+    language: string;
+    isbn10: string;
+    isbn13: string;
+    pages: string;
+    year: string;
+    rating: string;
+    desc: string;
+    price: string;
+    image: string;
+    url: string;
+    summary: number;
+}
+
+export interface IResponseSearch {
+    error: string;
+    total: string;
+    page: string;
+    books: IBook[];
+}
+
+export interface ISearchResult {
+    searchValue: string | undefined;
+    page: string | undefined;
+}
+
+export interface IBookISBN13 {
+    isbn13: string | undefined;
+}
