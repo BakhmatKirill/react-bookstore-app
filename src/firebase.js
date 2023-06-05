1815b83d46919f1a816215c08059c64ca1ea1c1a
111Bakhmat <k.bakhmat@mail.ru>
2023-06-05 21:10:53 +0300

    feat(src): add firebase.js file

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0jOJJeLkayrmfSo6Bq9Oj_mss6nOOdHQ",
+  authDomain: "fe-94-5e164.firebaseapp.com",
+  projectId: "fe-94-5e164",
+  storageBucket: "fe-94-5e164.appspot.com",
+  messagingSenderId: "192660976216",
+  appId: "1:192660976216:web:38f824f9a0444cf84f102d"
+};
+
+const app = initializeApp(firebaseConfig);
