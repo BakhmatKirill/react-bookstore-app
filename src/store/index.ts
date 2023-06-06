a9dea1d5a510bc85245071f2d3a112c90c84f222
111Bakhmat <k.bakhmat@mail.ru>
2023-06-06 20:51:40 +0300

    feat(store): add store selectors new, search, user and hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,17 @@
-//import arrowImage from "./icons/arrow.svg";
-//export { arrowImage };
+import { useAppDispatch } from "./hooks/useAppDispatch";
+import { useAppSelector } from "./hooks/useAppSelector";
+import { getNewBooks } from "./selectors/newBooksSelectors";
+import { fetchNewBooks } from "./slices/newBooksSlice";
+import { store } from "./store";
+import { feachSearchBooks } from "./slices/searchSlice";
+import { getSearchBooks } from "./selectors/searchBooksSelectors";
+
+export {
+  store,
+  useAppDispatch,
+  useAppSelector,
+  fetchNewBooks,
+  getNewBooks,
+  feachSearchBooks,
+  getSearchBooks,
+};
