53eb788f5480179d1000d99cd266b43033b3fe86
111Bakhmat <k.bakhmat@mail.ru>
2023-06-08 18:51:14 +0300

    feat(components): add component Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { StyledFooter, FooterText } from "./styles";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <StyledFooter>
+      <FooterText>©2022 Bookstore</FooterText>
+      <FooterText>All rights reserved</FooterText>
+    </StyledFooter>
+  );
 };
