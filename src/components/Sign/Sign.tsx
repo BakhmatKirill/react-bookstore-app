86e7cf0802f1095c899055de3b2f58d5784de824
111Bakhmat <k.bakhmat@mail.ru>
2023-06-26 21:29:30 +0300

    feat(components): add Sign component

--- src/components/Sign/Sign.tsx
+++ src/components/Sign/Sign.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { SignIn, SignUp } from "components";
+import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import { useState } from "react";
+import { SignItem, SignNavigation, StyledSign } from "./styles";
 
 export const Sign = () => {
-  return <div>Sign</div>;
+  const [isSign, setSign] = useState(true);
+
+  const handleSignIn = () => {
+    setSign(true);
+  };
+
+  const handleSignUp = () => {
+    setSign(false);
+  };
+
+  return (
+    <AnimateSharedLayout>
+      <StyledSign
+        layout
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.7, delay: 0.2 }}
+      >
+        <SignNavigation layout>
+          <SignItem onClick={handleSignIn} activeColor={isSign} activeBorder={isSign}>
+            Sign in
+          </SignItem>
+          <SignItem onClick={handleSignUp} activeColor={!isSign} activeBorder={!isSign}>
+            Sign up
+          </SignItem>
+        </SignNavigation>
+        <AnimatePresence initial={false}>{isSign ? <SignIn /> : <SignUp />}</AnimatePresence>
+      </StyledSign>
+    </AnimateSharedLayout>
+  );
 };
--- src/components/Sign/styles.ts
+++ src/components/Sign/styles.ts
@@ -1,1 +1,33 @@
-//
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { Color, Typography } from "ui";
+
+export const StyledSign = styled(motion.section)`
+  max-width: 544px;
+  margin: 0 auto 72px auto;
+
+  border: 1px solid ${Color.Gray};
+  overflow: hidden;
+`;
+
+export const SignNavigation = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+
+  height: 70px;
+  padding: 0 32px;
+  margin-bottom: 32px;
+
+  border-bottom: 1px solid ${Color.Gray};
+`;
+
+export const SignItem = styled.button<{ activeColor?: boolean; activeBorder?: boolean }>`
+  width: 50%;
+
+  ${Typography.H3};
+  color: ${({ activeColor }) => (activeColor ? Color.Primary : Color.Secondary)};
+
+  background: none;
+  border-bottom: ${({ activeBorder }) =>
+    activeBorder ? `2px solid ${Color.Primary}` : `2px solid ${Color.Primary_Background}`};
+`;
