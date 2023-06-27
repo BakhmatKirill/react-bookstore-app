8afc27f2c1f45801ba4572f5a73959a3e7695024
111Bakhmat <k.bakhmat@mail.ru>
2023-06-27 18:58:37 +0300

    fix(components): add adaptive to Account styles

--- src/components/Account/styles.ts
+++ src/components/Account/styles.ts
@@ -1,36 +1,93 @@
-import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import styled from "styled-components";
-import { Color, Typography } from "ui";
+import { Color, Typography, Media } from "ui";
 
-export const ProfileWrapper = styled.div`
+export const StyledAccount = styled.form`
+  flex: 1 0 auto;
+`;
+
+export const ProfileWrapper = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   column-gap: 32px;
+
   margin-bottom: 48px;
+
+  ${Media.MD} {
+    grid-template-columns: 1fr;
+  }
 `;
 
-export const PasswordWrapper = styled.div`
+export const PasswordWrapper = styled(motion.div)`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(1fr);
   column-gap: 32px;
 `;
 
-export const Subtitle = styled.h3`
+export const NewPasswordWrapper = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 32px;
+
+  ${Media.MD} {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const Subtitle = styled(motion.h3)`
   margin-bottom: 16px;
+
   ${Typography.H2}
   color: ${Color.Primary};
 `;
 
-export const StyledArrow = styled(Link)`
-  display: block;
-  margin-bottom: 32px;
-`;
-
-export const ButtonsWrapper = styled.div`
+export const ButtonsWrapper = styled(motion.div)`
   display: flex;
   justify-content: end;
   gap: 30px;
+
   padding-top: 48px;
   margin: 72px 0;
+
   border-top: 1px solid ${Color.Gray};
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledInput = styled.input`
+  max-width: 544px;
+  height: 56px;
+  padding: 10px 20px;
+  margin-bottom: 5px;
+
+  ${Typography.B1}
+
+  background: ${Color.Primary_Background};
+  border: 1px solid ${Color.Gray};
+
+  &::placeholder {
+    color: ${Color.Secondary};
+    text-transform: capitalize;
+  }
+
+  ${Media.EL} {
+    max-width: 456px;
+  }
+`;
+
+export const Label = styled.label`
+  padding-bottom: 10px;
+
+  ${Typography.B2};
+  color: ${Color.Primary};
+`;
+
+export const ErrorMessage = styled.p`
+  margin-bottom: 10px;
+
+  ${Typography.B1}
+  color: ${Color.Red};
 `;
