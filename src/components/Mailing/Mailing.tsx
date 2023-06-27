80b71f41b9a90e069f4841e893feeb46a72e906c
111Bakhmat <k.bakhmat@mail.ru>
2023-06-27 21:11:15 +0300

    feat(components): add logic to Mailing component

--- src/components/Mailing/Mailing.tsx
+++ src/components/Mailing/Mailing.tsx
@@ -1,17 +1,79 @@
+import axios from "axios";
 import { Button } from "components";
-import { Form, Input, StyledMailing, SubTitle, Title } from "./styles";
+import { useForm } from "react-hook-form";
+import { Form, Input, StyledMailing, SubTitle, Title, ErrorMessage } from "./styles";
+
+interface IMailing {
+  email: string;
+}
 
 export const Mailing = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = async (data: IMailing) => {
+    await axios.post("https://63b08824f9a53fa2026b2d9a.mockapi.io/users", {
+      email: data.email,
+    });
+    reset();
+  };
+
+  const mailingVariants = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.5,
+        duration: 0.8,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: -100,
+    },
+  };
+
   return (
-    <StyledMailing>
-      <Title>Subscribe to Newsletter</Title>
-      <SubTitle>
-        Be the first to know about new IT books, upcoming releases, exclusive offers and more.
-      </SubTitle>
-      <Form>
-        <Input type="email" placeholder="Your email" />
-        <Button type="button">Subscribe</Button>
-      </Form>
+    <StyledMailing
+      variants={mailingVariants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.2, once: true }}
+    >
+      {isSubmitSuccessful ? (
+        <Title>Thanks for subscribing to us!</Title>
+      ) : (
+        <>
+          <Title>Subscribe to Newsletter</Title>
+          <SubTitle>
+            Be the first to know about new IT books, upcoming releases, exclusive offers and more.
+          </SubTitle>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              autoComplete="off"
+              {...register("email", {
+                pattern: {
+                  value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                  message: "Enter the correct email!",
+                },
+                required: "Email is required field!",
+              })}
+              placeholder="Your email"
+              type="text"
+            />
+            <Button type="submit">Subscribe</Button>
+            {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+          </Form>
+        </>
+      )}
     </StyledMailing>
   );
 };
--- src/components/Mailing/styles.ts
+++ src/components/Mailing/styles.ts
@@ -1,11 +1,14 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Color, Typography, Media } from "ui";
 
-export const StyledMailing = styled.section`
+export const StyledMailing = styled(motion.section)`
   padding: 56px 64px;
   margin-bottom: 72px;
+
   background-color: ${Color.Secondary_Background};
   border-radius: 20px;
+
   ${Media.MD} {
     padding: 40px;
   }
@@ -15,8 +18,11 @@ export const StyledMailing = styled.section`
 `;
 
 export const Form = styled.form`
+  position: relative;
+
   display: grid;
   grid-template-columns: 4fr 1fr;
+
   ${Media.MD} {
     grid-template-columns: 3fr 1fr;
   }
@@ -29,21 +35,36 @@ export const Form = styled.form`
 
 export const Input = styled.input`
   padding: 16px 24px;
+
   ${Typography.B1};
+
   background: ${Color.Primary_Background};
 `;
 
 export const Title = styled.h2`
   max-width: 500px;
+
   margin-bottom: 8px;
+
   ${Typography.H2};
 `;
 
 export const SubTitle = styled.p`
   max-width: 500px;
   margin-bottom: 32px;
+
   ${Typography.B1};
+
   ${Media.SM} {
     max-width: 300px;
   }
+`;
+
+export const ErrorMessage = styled.p`
+  position: absolute;
+  bottom: -30px;
+  left: 0;
+
+  ${Typography.B1}
+  color: ${Color.Red};
 `;
