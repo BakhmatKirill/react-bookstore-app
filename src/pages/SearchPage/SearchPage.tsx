fe0863ecf74e4599b27e0da2ddcac48ed9f15308
111Bakhmat <k.bakhmat@mail.ru>
2023-06-29 20:05:57 +0300

    feat(pages): add Search page

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,3 +1,65 @@
+import { BackArrowButton, BooksList, Pagination, Title } from "components";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchSearchBooks, getSearchBooks, useAppDispatch, useAppSelector } from "store";
+import { SearchBooksWrapper, Subtitle } from "./styles";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const { page, searchValue } = useParams();
+  const navigate = useNavigate();
+
+  const { results, isLoading } = useAppSelector(getSearchBooks);
+  const { books, total } = results;
+  const dispatch = useAppDispatch();
+
+  const ITEMS_PER_PAGE = 10;
+  const pageCount = Math.ceil(+total / ITEMS_PER_PAGE);
+  const [itemOffset, setItemOffset] = useState(page);
+
+  useEffect(() => {
+    dispatch(
+      fetchSearchBooks({
+        searchValue: searchValue,
+        page: itemOffset,
+      }),
+    );
+  }, [searchValue, itemOffset, dispatch]);
+
+  const handlePageClick = (event: { selected: number }): void => {
+    const newOffset = String(event.selected + 1);
+
+    setItemOffset(() => {
+      return newOffset;
+    });
+
+    navigate(`/search/${searchValue}/${newOffset}`);
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <SearchBooksWrapper>
+      {isLoading && <Title text="Loading..." />}
+      {books && books.length > 0 ? (
+        <>
+          <Title text={`'${searchValue}' search results`} />
+          <Subtitle
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.7 }}
+          >{`Found ${total} books`}</Subtitle>
+          <BooksList books={books} />
+          <Pagination pageCount={pageCount} PageClick={handlePageClick} />
+        </>
+      ) : (
+        <>
+          <BackArrowButton />
+          <Title text="No results" />
+        </>
+      )}
+    </SearchBooksWrapper>
+  );
 };
--- src/pages/SearchPage/styles.ts
+++ src/pages/SearchPage/styles.ts
@@ -1,1 +1,15 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
+import { Color, Typography } from "ui";
+
+export const SearchBooksWrapper = styled.section`
+  flex: 1 0 auto;
+
+  margin-bottom: 72px;
+`;
+
+export const Subtitle = styled(motion.p)`
+  margin-bottom: 48px;
+  ${Typography.B1};
+  color: ${Color.Secondary};
+`;
