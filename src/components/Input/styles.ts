4f8c7ba598183c258d65d685ea5edd1a2dbe5077
111Bakhmat <k.bakhmat@mail.ru>
2023-06-29 19:49:17 +0300

    fix(components): fix Input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Typography, Color } from "ui";
+import { Typography, Color, Media } from "ui";
 
 export const InputWrapper = styled.div`
   display: flex;
@@ -8,22 +8,31 @@ export const InputWrapper = styled.div`
 
 export const Label = styled.label`
   margin-bottom: 10px;
+
   ${Typography.B2};
   color: ${Color.Primary};
 `;
 
 export const StyledInput = styled.input`
-
+  max-width: 544px;
   height: 56px;
   padding: 10px 20px;
   margin-bottom: 20px;
+
   ${Typography.B1}
+
   background: ${Color.Primary_Background};
   border: 1px solid ${Color.Gray};
+
   &::placeholder {
     color: ${Color.Secondary};
   }
-  &:focus{
+
+  &:focus {
     border: 2px solid ${Color.Gray};
+  }
+
+  ${Media.EL} {
+    max-width: 456px;
   }
 `;
