1aa8d363a540ddea29967982a9a17e669242ac54
111Bakhmat <k.bakhmat@mail.ru>
2023-07-01 10:48:29 +0300

    feat(components): add footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,11 +1,15 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Typography, Color, Media } from "ui";
 
-export const StyledFooter = styled.footer`
+export const StyledFooter = styled(motion.footer)`
   display: flex;
   justify-content: space-between;
+
   padding: 30px 0px;
+
   border-top: 1px solid ${Color.Gray};
+
   ${Media.SM} {
     flex-direction: column;
     align-items: center;
@@ -15,6 +19,7 @@ export const StyledFooter = styled.footer`
 export const FooterText = styled.p`
   ${Typography.B1}
   color: ${Color.Secondary};
+
   ${Media.SM} {
     margin-bottom: 24px;
   }
