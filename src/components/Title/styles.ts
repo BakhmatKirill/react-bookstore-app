9bdb0344a463504e407829134fdab59d6ae0c80c
111Bakhmat <k.bakhmat@mail.ru>
2023-07-01 11:38:44 +0300

    fix(components): fix title component

--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -1,9 +1,12 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Typography, Media } from "ui";
 
-export const StyledTitle = styled.h1`
+export const StyledTitle = styled(motion.h1)`
   margin-bottom: 40px;
+
   ${Typography.H1}
+
   ${Media.SM} {
     margin-bottom: 32px;
   }
